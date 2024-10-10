8992b40e5dc559ca80f98314a5ad3f30d2db3668
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-10-10 10:14:29 +0530

    add: ImageLoader

--- medrag_multi_modal/document_loader/__init__.py
+++ medrag_multi_modal/document_loader/__init__.py
@@ -1,4 +1,5 @@
+from .load_image import ImageLoader
 from .load_text import TextLoader
 from .load_text_image import TextImageLoader
 
-__all__ = ["TextLoader", "TextImageLoader"]
+__all__ = ["TextLoader", "TextImageLoader", "ImageLoader"]
