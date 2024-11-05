4482aadeef56a575fa4fae34da35a4615c1517be
soumik12345 <19soumik.rakshit96@gmail.com>
2024-11-05 08:33:11 +0530

    update: tests

--- tests/assistant/llm_client.py
+++ tests/assistant/llm_client.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from pydantic import BaseModel
 
 from medrag_multi_modal.assistant.llm_client import ClientType, LLMClient
@@ -9,7 +10,11 @@ class CalendarEvent(BaseModel):
     participants: list[str]
 
 
-def test_llm_client():
+class ImageDescription(BaseModel):
+    description: str
+
+
+def test_openai_llm_client():
     llm_client = LLMClient(model_name="gpt-4o-mini", client_type=ClientType.OPENAI)
     event = llm_client.predict(
         system_prompt="Extract the event information",
@@ -19,3 +24,13 @@ def test_llm_client():
     assert event.name.lower() == "science fair"
     assert event.date.lower() == "friday"
     assert [item.lower() for item in event.participants] == ["alice", "bob"]
+
+
+def test_openai_image_description():
+    llm_client = LLMClient(model_name="gpt-4o-mini", client_type=ClientType.OPENAI)
+    description = llm_client.predict(
+        system_prompt="Describe the image",
+        user_prompt=[Image.open("./assets/test_image.png")],
+        schema=ImageDescription,
+    )
+    assert "astronaut" in description.description.lower()
