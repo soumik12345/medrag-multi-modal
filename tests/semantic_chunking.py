5b1800faed0dbd763d0a19f8c938138b741129e5
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-11-06 13:33:39 +0530

    update: tests

--- tests/semantic_chunking.py
+++ tests/semantic_chunking.py
@@ -4,7 +4,7 @@ from medrag_multi_modal.semantic_chunking import SemanticChunker
 def test_semantic_chunking():
     chunker = SemanticChunker(chunk_size=256)
     dataset = chunker.chunk(document_dataset="geekyrakshit/grays-anatomy-test")
-    assert dataset.num_rows == 120
+    assert dataset.num_rows == 49
     assert dataset.column_names == [
         "document_idx",
         "text",
