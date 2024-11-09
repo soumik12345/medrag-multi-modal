aba310ec22375b5d48a1c781ac35ead872b778fb
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-11-09 23:14:39 +0530

    update: app.py

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import streamlit as st
-import weave
 
-from medrag_multi_modal.assistant import (
-    FigureAnnotatorFromPageImage,
-    LLMClient,
-    MedQAAssistant,
+from medrag_multi_modal.assistant import LLMClient, MedQAAssistant
+from medrag_multi_modal.retrieval.text_retrieval import (
+    BM25sRetriever,
+    ContrieverRetriever,
+    MedCPTRetriever,
+    NVEmbed2Retriever,
 )
-from medrag_multi_modal.assistant.llm_client import (
-    GOOGLE_MODELS,
-    MISTRAL_MODELS,
-    OPENAI_MODELS,
-)
-from medrag_multi_modal.retrieval import MedCPTRetriever
 
 # Define constants
-ALL_AVAILABLE_MODELS = GOOGLE_MODELS + MISTRAL_MODELS + OPENAI_MODELS
+ALL_AVAILABLE_MODELS = [
+    "gemini-1.5-flash-latest",
+    "gemini-1.5-pro-latest",
+    "gpt-4o",
+    "gpt-4o-mini",
+]
 
 # Sidebar for configuration settings
 st.sidebar.title("Configuration Settings")
@@ -24,68 +24,91 @@ project_name = st.sidebar.text_input(
     placeholder="wandb project name",
     help="format: wandb_username/wandb_project_name",
 )
-chunk_dataset_name = st.sidebar.text_input(
-    label="Text Chunk WandB Dataset Name",
-    value="grays-anatomy-chunks:v0",
-    placeholder="wandb dataset name",
-    help="format: wandb_dataset_name:version",
+chunk_dataset_id = st.sidebar.selectbox(
+    label="Chunk Dataset ID",
+    options=["ashwiniai/medrag-text-corpus-chunks"],
 )
-index_artifact_address = st.sidebar.text_input(
-    label="WandB Index Artifact Address",
-    value="ml-colabs/medrag-multi-modal/grays-anatomy-medcpt:v0",
-    placeholder="wandb artifact address",
-    help="format: wandb_username/wandb_project_name/wandb_artifact_name:version",
-)
-image_artifact_address = st.sidebar.text_input(
-    label="WandB Image Artifact Address",
-    value="ml-colabs/medrag-multi-modal/grays-anatomy-images-marker:v6",
-    placeholder="wandb artifact address",
-    help="format: wandb_username/wandb_project_name/wandb_artifact_name:version",
-)
-llm_client_model_name = st.sidebar.selectbox(
-    label="LLM Client Model Name",
+llm_model = st.sidebar.selectbox(
+    label="LLM Model",
     options=ALL_AVAILABLE_MODELS,
-    index=ALL_AVAILABLE_MODELS.index("gemini-1.5-flash"),
-    help="select a model from the list",
 )
-figure_extraction_model_name = st.sidebar.selectbox(
-    label="Figure Extraction Model Name",
-    options=ALL_AVAILABLE_MODELS,
-    index=ALL_AVAILABLE_MODELS.index("pixtral-12b-2409"),
-    help="select a model from the list",
+top_k_chunks_for_query = st.sidebar.slider(
+    label="Top K Chunks for Query",
+    min_value=1,
+    max_value=20,
+    value=5,
 )
-structured_output_model_name = st.sidebar.selectbox(
-    label="Structured Output Model Name",
-    options=ALL_AVAILABLE_MODELS,
-    index=ALL_AVAILABLE_MODELS.index("gpt-4o"),
-    help="select a model from the list",
+top_k_chunks_for_options = st.sidebar.slider(
+    label="Top K Chunks for Options",
+    min_value=1,
+    max_value=20,
+    value=3,
 )
-
-# Streamlit app layout
-st.title("MedQA Assistant App")
-
-# Initialize Weave
-weave.init(project_name=project_name)
-
-# Initialize clients and assistants
-llm_client = LLMClient(model_name=llm_client_model_name)
-retriever = MedCPTRetriever.from_wandb_artifact(
-    chunk_dataset_name=chunk_dataset_name,
-    index_artifact_address=index_artifact_address,
+rely_only_on_context = st.sidebar.checkbox(
+    label="Rely Only on Context",
+    value=False,
 )
-figure_annotator = FigureAnnotatorFromPageImage(
-    figure_extraction_llm_client=LLMClient(model_name=figure_extraction_model_name),
-    structured_output_llm_client=LLMClient(model_name=structured_output_model_name),
-    image_artifact_address=image_artifact_address,
-)
-medqa_assistant = MedQAAssistant(
-    llm_client=llm_client, retriever=retriever, figure_annotator=figure_annotator
+retriever_type = st.sidebar.selectbox(
+    label="Retriever Type",
+    options=[
+        "",
+        "BM25S",
+        "Contriever",
+        "MedCPT",
+        "NV-Embed-v2",
+    ],
 )
 
-query = st.chat_input("Enter your question here")
-if query:
-    with st.chat_message("user"):
-        st.markdown(query)
-    response = medqa_assistant.predict(query=query)
+if retriever_type != "":
+
+    llm_model = LLMClient(model_name=llm_model)
+
+    retriever = None
+
+    if retriever_type == "BM25S":
+        retriever = BM25sRetriever.from_index(
+            index_repo_id="ashwiniai/medrag-text-corpus-chunks-bm25s"
+        )
+    elif retriever_type == "Contriever":
+        retriever = ContrieverRetriever.from_index(
+            index_repo_id="ashwiniai/medrag-text-corpus-chunks-contriever",
+            chunk_dataset_id=chunk_dataset_id,
+        )
+    elif retriever_type == "MedCPT":
+        retriever = MedCPTRetriever.from_index(
+            index_repo_id="ashwiniai/medrag-text-corpus-chunks-medcpt",
+            chunk_dataset_id=chunk_dataset_id,
+        )
+    elif retriever_type == "NV-Embed-v2":
+        retriever = NVEmbed2Retriever.from_index(
+            index_repo_id="ashwiniai/medrag-text-corpus-chunks-nv-embed-2",
+            chunk_dataset_id=chunk_dataset_id,
+        )
+
+    medqa_assistant = MedQAAssistant(
+        llm_client=llm_model,
+        retriever=retriever,
+        top_k_chunks_for_query=top_k_chunks_for_query,
+        top_k_chunks_for_options=top_k_chunks_for_options,
+    )
+
     with st.chat_message("assistant"):
-        st.markdown(response)
+        st.markdown(
+            """
+Hi! I am Medrag, your medical assistant. You can ask me any questions about the medical and the life sciences.
+I am currently a work-in-progress, so please bear with my stupidity and overall lack of knowledge.
+
+**Note:** that I am not a medical professional, so please do not rely on my answers for medical decisions.
+Please consult a medical professional for any medical advice.
+
+In order to learn more about how I am being developed, please visit [soumik12345/medrag-multi-modal](https://github.com/soumik12345/medrag-multi-modal).
+            """,
+            unsafe_allow_html=True,
+        )
+    query = st.chat_input("Enter your question here")
+    if query:
+        with st.chat_message("user"):
+            st.markdown(query)
+        response = medqa_assistant.predict(query=query)
+        with st.chat_message("assistant"):
+            st.markdown(response.response)
