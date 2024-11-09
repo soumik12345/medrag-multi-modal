4e97f576c9cb4920668c699b4b276fc86aef941d
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-11-09 23:20:58 +0530

    fix: bug in using neural retrievers in app

--- app.py
+++ app.py
@@ -72,17 +72,17 @@ if retriever_type != "":
     elif retriever_type == "Contriever":
         retriever = ContrieverRetriever.from_index(
             index_repo_id="ashwiniai/medrag-text-corpus-chunks-contriever",
-            chunk_dataset_id=chunk_dataset_id,
+            chunk_dataset=chunk_dataset_id,
         )
     elif retriever_type == "MedCPT":
         retriever = MedCPTRetriever.from_index(
             index_repo_id="ashwiniai/medrag-text-corpus-chunks-medcpt",
-            chunk_dataset_id=chunk_dataset_id,
+            chunk_dataset=chunk_dataset_id,
         )
     elif retriever_type == "NV-Embed-v2":
         retriever = NVEmbed2Retriever.from_index(
             index_repo_id="ashwiniai/medrag-text-corpus-chunks-nv-embed-2",
-            chunk_dataset_id=chunk_dataset_id,
+            chunk_dataset=chunk_dataset_id,
         )
 
     medqa_assistant = MedQAAssistant(
